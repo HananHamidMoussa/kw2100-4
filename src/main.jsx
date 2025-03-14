1433dfb54135bd1b64c113357e3174641a5b702e
HananHamidMoussa <Hamo031@student.kristiania.no>
2025-03-14 11:39:40 +0100

    Initial React application

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import {createRoot} from "react-dom/client";
+
+
+createRoot(document.getElementById("Johannes")).render(<h1>Velkommen til emnet KWS - 2100 Oppgave 4</h1>)
