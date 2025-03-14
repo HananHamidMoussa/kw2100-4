4bfe7c505ad7d340b0133812738ed66241890b5d
HananHamidMoussa <Hamo031@student.kristiania.no>
2025-03-14 11:57:41 +0100

    Initial prettier as a test command

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import {createRoot} from "react-dom/client";
+import { createRoot } from "react-dom/client";
 
-
-createRoot(document.getElementById("Johannes")).render(<h1>Velkommen til emnet KWS - 2100 Oppgave 4</h1>)
+createRoot(document.getElementById("Johannes")).render(
+  <h1>Velkommen til emnet KWS - 2100 Oppgave 4</h1>,
+);
